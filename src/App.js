9486556ddb9d5b9484bdb3b965a6fc6a2e250339
Raul Villora Valencia <r.vvalencia@alumnos.upm.es>
2019-02-18 12:31:18 +0100

    Added Welcome Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-// import { Link } from 'react-router-dom';
+ import { Link } from 'react-router-dom';
 import Main from './components/main';
 
 export default class App extends Component {
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,32 +6,29 @@ import { Link } from 'react-router-dom';
 export default class LandingPage extends Component {
     render = () => {
         return (
-            <div className="heading">
-                <div class="stage">
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <Link to="/2048">Click here to</Link>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                </div>
-
+            <div class="headingStyle">
+                <Link to="/2048">
+                    <h1>
+                        <span>W</span>
+                        <span>e</span>
+                        <span>l</span>
+                        <span>c</span>
+                        <span>o</span>
+                        <span>m</span>
+                        <span>e</span>
+                        <br></br>
+                        <span>t</span>
+                        <span>o</span>
+                        <br></br>
+                        <span>2</span>
+                        <span>0</span>
+                        <span>4</span>
+                        <span>8</span>
+                        <span>!</span>
+                    </h1>
+                </Link>
             </div>
+
         );
     }
 }
