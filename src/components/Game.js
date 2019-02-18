9fcf83b725d0ca48af7ad5ffd285cb2717c79046
Raul Villora Valencia <r.vvalencia@alumnos.upm.es>
2019-02-18 17:26:09 +0100

    First step of creating the game

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,8 +5,49 @@ import '../App.css';
 export default class Game extends Component {
     render = () => {
         return (
-            <div>
-                <p>2048</p>
+            <div className="container">
+                <div className="gameHeading">
+                    <div className="newGame">
+                        New Game
+                    </div>
+                    <div className="scorer-value">
+                        0
+                    </div>
+                    <div className="hint">
+                        <p>HINT</p> {/* HERE it is need to call a function to know which is the next move and then (or move it or show the player the optimal next move!) */}
+                    </div>
+
+                </div>
+
+                <div className="gameContainer">
+
+                    <table cellspacing="4" className="dashboard">
+                        <tr>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                        </tr>
+                        <tr>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                        </tr>
+                        <tr>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                        </tr>
+                        <tr>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                            <td className="cell"></td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         );
     }
