fdab153d8bc4da4bdbe3e85abb90ed19f24a08f2
Raul Villora Valencia <r.vvalencia@alumnos.upm.es>
2019-02-19 11:19:20 +0100

    Change CSS game

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,18 +5,24 @@ import '../App.css';
 export default class Game extends Component {
     render = () => {
         return (
-            <div className="container">
-                <div className="gameHeading">
-                    <div className="newGame">
-                        New Game
-                    </div>
-                    <div className="scorer-value">
-                        0
-                    </div>
-                    <div className="hint">
-                        <p>HINT</p> {/* HERE it is need to call a function to know which is the next move and then (or move it or show the player the optimal next move!) */}
-                    </div>
 
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm">
+                            <button type="button" className="btn btn-primary" style={{ margin: '10px 10px', fontFamily: "Avenir" }}>New Game</button>
+                    </div>
+                    <div className="col-sm">
+                        <div className="scorer">
+                            <div className="scorerValue">
+                                0
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                            <button type="button" className="btn btn-primary" style={{ margin: '10px 10px', fontFamily: "Avenir" }}>HINT</button>
+                            {/* HERE it is need to call a function to know which is the next move and then (or move it or show the player the optimal next move!) */}
+                    </div>
                 </div>
 
                 <div className="gameContainer">
