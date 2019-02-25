914316815e414c9f9b4ce7a12cec137cb50fc54b
Raul Villora Valencia <r.vvalencia@alumnos.upm.es>
2019-02-25 13:40:47 +0100

    Updates

--- precache-manifest.d7103e00af9c80c10e19b38add536c10.js
+++ precache-manifest.d7103e00af9c80c10e19b38add536c10.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ef39be3bfb013527febe",
+    "revision": "d1e2a80dcc9056772b5c",
     "url": "/2048/static/css/main.131a56c6.chunk.css"
   },
   {
-    "revision": "ef39be3bfb013527febe",
-    "url": "/2048/static/js/main.ef39be3b.chunk.js"
+    "revision": "d1e2a80dcc9056772b5c",
+    "url": "/2048/static/js/main.d1e2a80d.chunk.js"
   },
   {
     "revision": "db00710998a9f0cd2fd8",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/2048/static/js/2.345e5616.chunk.js"
   },
   {
-    "revision": "883edbd099b6f8b4a7d5645c5b3ce35b",
+    "revision": "55f94543bf20c5049e0bcab82743e31e",
     "url": "/2048/index.html"
   }
 ];
