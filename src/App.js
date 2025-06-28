9372cee3861959ca71d2045181e1948d89643c24
Raul Villora Valencia <dyw@qlik.com>
2025-06-28 12:50:03 +0200

    chore: remove landing page and set game as landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import React, { useState } from "react";
 import "./App.css";
-import LandingPage from "./components/LandingPage";
 import Game from "./components/Game";
 
 export default function App() {
-  const [landingPageHidden, setLandingPageHidden] = useState(false);
-  const [gameHidden, setGameHidden] = useState(true);
-
-  const handleLandingPageClick = () => {
-    setLandingPageHidden(true);
-    setGameHidden(false);
-  };
-
   return (
     <div className="App" style={{ touchAction: "manipulation" }}>
-      {!landingPageHidden && (
-        <div className="headingStyle">
-          <LandingPage handleLandingPageClick={handleLandingPageClick} />
-        </div>
-      )}
-      {!gameHidden && <Game />}
+      <Game />
     </div>
   );
 }
